16f611c45e0fc4e90cfab917e570b928284d1076
spectre1011 <aman.gaur1011@gmail.com>
2016-09-02 16:18:28 +0530

    checking jQuery and hiding the paragraph

--- dashboard.pl
+++ dashboard.pl
@@ -9,15 +9,28 @@ my $html = qq{
 		<head>
 			<title>Theseus Statistics Dashboard</title>
 			<style> 
-                p {
+                #heading {
 		            text-align: center;
 		            font-family: serif, sans-serif ,times;
 		            font-size: 50px;
 	            }
 	        </style>
+	        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	        <script>
+	        	jQuery(document).ready(function() {
+	        		jQuery("button").click(function() {
+	        			jQuery("#tohide").hide();
+	        		});
+	        	});
+	        </script>
+
 		</head>
 		<body> 
-			<p> Theseus Metrics </p>
+			<p id='heading'> Theseus Metrics </p>
+			<button>Click me to hide the hide paragraph.</button>
+			<p id="tohide"> This is to hide</p>
+
+
 		</body>
 };
 
