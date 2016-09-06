c4d0b44743fc73b59e1ee64d74e5b33936f638bc
spectre1011 <aman.gaur1011@gmail.com>
2016-09-06 12:51:29 +0530

    Adding the table style elements.

--- dashboard.pl
+++ dashboard.pl
@@ -4,6 +4,10 @@ use warnings;
 
 no warnings 'uninitialized';
 
+use Athena ::Lib qw/:athena :intranet/;
+use AthenaUtils;
+
+
 my $html = qq{
 	<html>
 		<head>
@@ -14,6 +18,56 @@ my $html = qq{
 		            font-family: serif, sans-serif ,times;
 		            font-size: 50px;
 	            }
+
+	            .showtable {
+	            	color: #fff;
+	            	font-family: Century Gothic;
+	            	width: 90%;
+	            	border-collapse: collapse;
+	            	border-spacing: 0;
+	            }
+
+	            .showtable table,th,td {
+	            	text-align: center;
+	            	font-size: 20px;
+	            }
+
+	            .showtable th{
+	            	background: #61B329;
+	            	color: #fff;
+	            	font-weight: bold;
+	            	font-size: 20px;
+	            	font-family: Century Gothic;
+	            	vertical-align: middle;
+	            }
+
+	            .showtable td{
+	            	border: 1px solid transparent;
+	            	height: 30px;
+	            	transition: all 0.3s;
+	            	background: #FAFAFA;
+	            	text-align: center;
+	            }
+
+	            .showtable #project {
+	            	width: 80%;
+	            	text-align: left;
+	            }
+
+	            .showtable tr:nth-child(even) td {
+	            	background: #F1F1F1;
+	            }
+
+	            .showtable tr:nth-child(odd) td {
+	            	background: #FEFEFE;
+	            }
+
+	            .showtable tr td:hover {
+	            	background: #664088;
+	            	color: #FFF;
+	            	cursor:pointer;
+	            }
+
 	        </style>
 	        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	        <script>
@@ -33,6 +87,7 @@ my $html = qq{
 			<div align='center' id='result'>
 				<button type='button'>Click Me!</button>
 			</div>
+			<div class='showtable' align='center'>
 		</body>
 	</html>
 };
